26cb4a5cc0029f15a5922abd775398f4a39d9957
özge kilic <ozgeklc096@gmail.com>
2023-09-25 20:27:56 +0200

    component state

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,19 +4,23 @@ import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import { Component } from 'react';
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 class App extends Component {
+
   render () {
     return (
       <div className="App">
-        <Greet name='Bruce' heroName="Batman">
+        <Message />
+
+      {/*   <Greet name='Bruce' heroName="Batman">
           <p>This is children props</p>
         </Greet>
         <Greet name='Clark' heroName="Superman"/>
           <button> Action </button>
         <Greet name='Diana' heroName="Princess"/>
         <Welcome name='Clark' heroName="Superman"/>
-        
+         */}
        {/*  <Welcome/> */} */
         {/* <Hello/> */}
       </div>
