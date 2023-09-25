98fc74ad30733934ee395f2ac21ed43c2b4d7ffb
özge kilic <ozgeklc096@gmail.com>
2023-09-25 17:13:50 +0200

    jsx beginşng

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './components/Greet;'
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
 import { Component } from 'react';
-
+import Hello from './components/Hello'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <MyComponent />
+        {/* <Greet/>
+        <Welcome/> */}
+        <Hello/>
       </div>
     )
   }
