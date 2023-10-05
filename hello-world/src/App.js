358180d3686f4222077ebdbfde92d552453dcda6
özge kilic <ozgeklc096@gmail.com>
2023-10-05 13:13:09 +0200

    props setState and count

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,16 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
-import { Component } from 'react';
+import React, { Component } from 'react'
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 class App extends Component {
 
   render () {
     return (
       <div className="App">
-        <Message />
+        <Counter />
+       {/*  <Message /> */}
 
       {/*   <Greet name='Bruce' heroName="Batman">
           <p>This is children props</p>
