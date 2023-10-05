c11452c98273f10a60b1a92295f40488302a85d5
özge kilic <ozgeklc096@gmail.com>
2023-10-05 15:16:49 +0200

    functıon hsndler and class handler

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,18 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import React, { Component } from 'react'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick';
+
 
 class App extends Component {
 
   render () {
     return (
       <div className="App">
-        <Counter />
+        <FunctionClick />
+      {/* <Counter /> */}
        {/*  <Message /> */}
 
       {/*   <Greet name='Bruce' heroName="Batman">
