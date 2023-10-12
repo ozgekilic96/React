ec0586dc2cdcc821ffb2b7f6bcef1e05a48734c7
özge kilic <ozgeklc096@gmail.com>
2023-10-12 17:20:42 +0200

    useState Hooks

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,56 +1,25 @@
-import logo from './logo.svg'
+
 import './App.css'
-import Greet from './components/wishas/Greet'
-import Welcome from './components/wishas/Welcome'
-import React, { Component } from 'react'
-import Hello from './components/wishas/Hello'
-import Message from './components/wishas/Message'
-import Counter from './components/wishas/Counter'
-import FunctionClick from './components/wishas/FunctionClick'
-import ClassClick from './components/wishas/ClassClick'
-import EventBind from './components/wishas/EventBind'
-import ParentComponent from './components/wishas/ParentComponent'
-import ChildComponent from './components/wishas/ChildComponent'
-import UserGreeting from './components/wishas/UserGreeting'
+import React, { useState } from 'react'
 import GoalList from './components/GoalList'
 import NewGoal from './components/NewGoal'
 
-
   const App = () => {
-      const courseGoals = [
-        {id: "cg1", text:"Finish your course"},
-        {id: "cg2", text:"Learn about all the topis "},
-        {id: "cg3", text:"Help other students"},
-      ]
-      const addNewGoalHandler = (newGoal) => {
-        courseGoals.push(newGoal)
-        console.log(courseGoals)
-      }
+    const [courseGoals, setCourseGoals] = useState([
+      {id: "cg1", text:"Finish your course"},
+      {id: "cg2", text:"Learn about all the topis "},
+      {id: "cg3", text:"Help other students"},
+    ])
+    const addNewGoalHandler = newGoal => {
+      setCourseGoals(courseGoals.concat(newGoal))
+    }
 
 
     return (
       <div>
-        <h2 className="header">Class Goals</h2>
-        <NewGoal onAddGoal={addNewGoalHandler} />
-        <GoalList goals={courseGoals} />
-
-        {/* < UserGreeting /> */}
-        {/* <ParentComponent /> */}
-        {/* <EventBind /> */}
-        {/* <FunctionClick /> */}
-        {/* <Counter /> */}
-         {/*  <Message /> */}
-
-        {/*   <Greet name='Bruce' heroName="Batman">
-          <p>This is children props</p>
-        </Greet>
-        <Greet name='Clark' heroName="Superman"/>
-          <button> Action </button>
-        <Greet name='Diana' heroName="Princess"/>
-        <Welcome name='Clark' heroName="Superman"/>
-         */}
-       {/*  <Welcome/> */} */
-        {/* <Hello/> */}
+          <h2 className="header">Class Goals</h2>
+          <NewGoal onAddGoal={addNewGoalHandler} />
+          <GoalList goals={courseGoals} />
       </div>
     );
   };
