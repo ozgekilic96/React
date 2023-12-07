463edce35cbd0e5f3a2bf3bc95bb3e8204f4c8f3
özge kilic <ozgeklc096@gmail.com>
2023-12-07 22:15:03 +0100

    components

---
+++ React-Essentials/src/components/CoreConcept.jsx
@@ -0,0 +1,9 @@
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title} </h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
