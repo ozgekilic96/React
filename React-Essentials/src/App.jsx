09d68c8201df0be29804f3ad1d80ec19e9c753e4
özge kilic <ozgeklc096@gmail.com>
2023-12-18 16:26:58 +0100

    dynamic styling

--- React-Essentials/src/App.jsx
+++ React-Essentials/src/App.jsx
@@ -8,7 +8,7 @@ import TabButton from "./components/TabButton";
 import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
@@ -35,20 +35,22 @@ function App() {
       <section id="examples">
         <h2> Examples </h2>
         <menu>
-          <TabButton onSelect={() => handleSelect("component")}>
-            Components
-          </TabButton>
-          <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-          <TabButton onSelect={() => handleSelect("props")}>props</TabButton>
-          <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect("components")}>Component</TabButton>
+          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+          <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect("props")}>props</TabButton>
+          <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect("state")}>State</TabButton>
         </menu>
-        <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p> {EXAMPLES[selectedTopic].description} </p>
-          <pre>
-            <code>{EXAMPLES[selectedTopic].code} </code>
-          </pre>
-        </div>
+        {!selectedTopic ? <p> Please selecet a topic </p> : null}
+        {selectedTopic ? (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p> {EXAMPLES[selectedTopic].description} </p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code} </code>
+            </pre>
+          </div>
+        ) : null}
+        )
       </section>
     </div>
   );
