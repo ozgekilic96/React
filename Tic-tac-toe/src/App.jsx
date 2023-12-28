374b4c5b77ac2ec89933263ea94c61ab08ab37b8
özge kilic <ozgeklc096@gmail.com>
2023-12-28 19:38:54 +0100

    x and o

--- Tic-tac-toe/src/App.jsx
+++ Tic-tac-toe/src/App.jsx
@@ -1,20 +1,37 @@
-import Player from './components/Player.jsx'
-import GameBoard from './components/GameBoard.jsx'
+import { useState } from "react";
+
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
 
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "0" : "X"));
+  }
 
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X"/>
-          <Player initialName="Player 2" symbol="O"/>
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="0"
+            isActive={activePlayer === "0"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- Tic-tac-toe/src/components/Player.jsx
+++ Tic-tac-toe/src/components/Player.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol, isActive }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -9,10 +9,10 @@ export default function Player({ initialName, symbol }) {
   }
 
   function handleChange(event) {
-    setPlayerName(event.target.value)
+    setPlayerName(event.target.value);
   }
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {!isEditing ? (
           <>
@@ -20,10 +20,15 @@ export default function Player({ initialName, symbol }) {
             <span className="player-symbol">{symbol}</span>
           </>
         ) : (
-          <input type="text" required value={playerName} onChange={handleChange}/>
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
         )}
       </span>
-      <button onClick={handleClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
