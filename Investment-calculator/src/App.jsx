eafe5d82d2fea39b4d8f5b03404ff4004fde8f74
özge kilic <ozgeklc096@gmail.com>
2024-01-07 15:52:50 +0100

    first part finished

--- Investment-calculator/src/App.jsx
+++ Investment-calculator/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
-import Results from './components/Result.jsx'
+import Results from './components/Results.jsx'
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -14,7 +14,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
