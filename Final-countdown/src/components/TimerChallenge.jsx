5d91253d5c0ae51930a585c1fb40f6dc5c5c2e01
özge kilic <ozgeklc096@gmail.com>
2024-01-10 17:16:30 +0100

    setTimer useRef

--- Final-countdown/src/components/TimerChallenge.jsx
+++ Final-countdown/src/components/TimerChallenge.jsx
@@ -1,18 +1,25 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
 
+
   function handleStart() {
+
     setTimerStarted(true);
-    setTimeout(() => {
+
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
   }
 
   function handleStop() {
-    
+    clearTimeout(timer.current);
   }
   return (
     <section className="challenge">
@@ -21,7 +28,7 @@ export default function TimerChallenge({ title, targetTime }) {
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
-      <button onClick={handleStart}>
+      <button onClick={timerStarted ? handleStop : handleStart}>
         {timerStarted ? "Stop" : "Start"} Challenge
       </button>
       <p className={timerStarted ? "active" : undefined}>
