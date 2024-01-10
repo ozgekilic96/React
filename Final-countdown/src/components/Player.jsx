36bea0a1e0cc20bc1b62ea334cc7d322cafb3ac1
özge kilic <ozgeklc096@gmail.com>
2024-01-10 14:59:33 +0100

    useRef

--- Final-countdown/src/components/Player.jsx
+++ Final-countdown/src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from 'react'
+
 export default function Player() {
+  const player = useRef();
+
+  const[playerName, setPlayerName] = useState('');
+
+
+  function handleClick() {
+    setPlayerName(player.current.value);
+    player.current.value = '';
+
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : "unknown entity"} </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={player} type="text" />
+        <button onClick={handleClick} >Set Name</button>
       </p>
     </section>
   );
