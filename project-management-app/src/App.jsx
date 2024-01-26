9d712e351f304fd11e383ac762faab5b4ef2db5c
özge kilic <ozgeklc096@gmail.com>
2024-01-26 13:20:05 +0100

    side bar and input

--- project-management-app/src/App.jsx
+++ project-management-app/src/App.jsx
@@ -1,8 +1,12 @@
+import ProjectSideBar from './components/ProjectSideBar.jsx'
+import NewProject from './components/NewProject.jsx'
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+   <main  className="h-screen my-8 flex gap-8">
+      <ProjectSideBar />
+      <NewProject />
+   </main>
   );
 }
 
