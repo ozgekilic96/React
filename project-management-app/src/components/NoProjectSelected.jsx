179ec4320fbb3a507af577adac612cc800a430e6
özge kilic <ozgeklc096@gmail.com>
2024-02-16 14:27:42 +0100

    continue later

--- project-management-app/src/components/NoProjectSelected.jsx
+++ project-management-app/src/components/NoProjectSelected.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import from './Button.jsx'
+import noProjectImage from '../assests/no-projects.png'
 
 export default function NoProjectSelected() {
   return (
-    <div>
-      
+    <div className="mt-24 text-center w-2/3">
+      <img
+      src={noProjectImage}
+      alt="An empty task list"
+      className="w-16 h-16 object-contain" />
+      <h2 className="text-xl font-bold text-stone-500 my-4">No project Selected</h2>
+      <p> Select a project or get started with a new one</p>
+      <Button>Create new project</Button>
     </div>
   )
 }
